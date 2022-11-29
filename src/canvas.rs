132cb6da5c8d3da25646089003f2260ffc2f26f1
Alex Sage <sagezander@gmail.com>
2022-11-28 16:37:48 -0800

    fix: canvas now renders top line

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,7 +1,7 @@
 use crate::{point::Point, state::State, term};
 
 pub async fn render_canvas(state: &State) {
-  let canvas_dimensions_start = (state.brush_menu_width, 1);
+  let canvas_dimensions_start = (state.brush_menu_width, 0);
   let canvas_dimensions_end = state.terminal_size;
   if let Some(pos) = state.mouse_events.left_pressed {
     if pos.is_inbetween(canvas_dimensions_start, canvas_dimensions_end) {
