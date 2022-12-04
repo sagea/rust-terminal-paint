533cf84df2228ae740ff60caeed78eff945262a3
Alex Sage <sagezander@gmail.com>
2022-12-03 18:28:18 -0800

    fix: breaks in brush line

--- src/line_processor.rs
+++ src/line_processor.rs
@@ -1,70 +1,103 @@
 use crate::point::Point;
 
-pub fn plot_line_low(x0: i16, y0: i16, x1: i16, y1: i16) -> Vec<Point> {
-  let mut pts = vec![];
-  let dx = x1 - x0;
-  let mut dy = y1 - y0;
-  let mut yi = 1;
-  if dy < 0 {
-    yi = -1;
-    dy = -dy;
-  }
-  let mut d = (2 * dy) - dx;
-  let mut y = y0;
-
-  for x in x0..x1 {
-    pts.push(Point::new(x as u16, y as u16));
-    if d > 0 {
-      y += yi;
-      d += 2 * (dy - dx);
-    } else {
-      d += 2 * dy;
-    }
-  }
-  pts
-}
-
-pub fn plot_line_high(x0: i16, y0: i16, x1: i16, y1: i16) -> Vec<Point> {
-  let mut pts = vec![];
-  let mut dx = x1 - x0;
-  let dy = y1 - y0;
-  let mut xi = 1;
-  if dx < 0 {
-    xi = -1;
-    dx = -dx;
-  }
-  let mut d = (2 * dx) - dy;
-  let mut x = x0;
-
-  for y in y0..y1 {
-    pts.push(Point::new(x as u16, y as u16));
-    if d > 0 {
-      x += xi;
-      d += 2 * (dx - dy);
-    } else {
-      d += 2 * dx
-    }
-  }
-  pts
+pub fn get_increment(iterations: f32, a: f32, b: f32) -> f32 {
+  let diff = b - a;
+  diff / iterations
 }
 
 pub fn plot_line(start: Point, end: Point) -> Vec<Point> {
-  let x0 = start.x as i16;
-  let y0 = start.y as i16;
-  let x1 = end.x as i16;
-  let y1 = end.y as i16;
+  let x0 = start.x as f32;
+  let y0 = start.y as f32;
+  let x1 = end.x as f32;
+  let y1 = end.y as f32;
+  let mut list = vec![];
   if x0 == x1 && y0 == y1 {
     return vec![start];
   }
-  if (y1 - y0).abs() < (x1 - x0).abs() {
-    if x0 > x1 {
-      plot_line_low(x1, y1, x0, y0)
-    } else {
-      plot_line_low(x0, y0, x1, y1)
-    }
-  } else if y0 > y1 {
-    plot_line_high(x1, y1, x0, y0)
-  } else {
-    plot_line_high(x0, y0, x1, y1)
+  let x_iterations = (x0 - x1).abs();
+  let y_iterations = (y0 - y1).abs();
+  let total_iterations = x_iterations.max(y_iterations);
+  let x_slope = get_increment(total_iterations, x0, x1);
+  let y_slope = get_increment(total_iterations, y0, y1);
+  let mut i = 0;
+  while i <= total_iterations as i32 {
+    let x = x0 + (i as f32 * x_slope).round();
+    let y = y0 + (i as f32 * y_slope).round();
+
+    list.push(Point::new(x as u16, y as u16));
+    i += 1;
+  }
+  list
+}
+
+#[cfg(test)]
+mod tests {
+  use super::plot_line;
+  use crate::point::Point;
+
+  #[test]
+  pub fn should_draw_line_inbetween_points_1() {
+    let start = Point::new(60, 29);
+    let end = Point::new(62, 27);
+    let result = plot_line(start, end);
+    let expected_result = vec![Point::new(60, 29), Point::new(61, 28), Point::new(62, 27)];
+
+    assert_eq!(result, expected_result, "Should have been equal");
+  }
+  #[test]
+  pub fn should_draw_line_inbetween_points_2() {
+    let start = Point::new(50, 50);
+    let end = Point::new(53, 53);
+    let result = plot_line(start, end);
+    let expected_result = vec![
+      Point::new(50, 50),
+      Point::new(51, 51),
+      Point::new(52, 52),
+      Point::new(53, 53),
+    ];
+
+    assert_eq!(result, expected_result, "Should have been equal");
+  }
+
+  #[test]
+  pub fn should_draw_line_inbetween_points_3() {
+    let start = Point::new(50, 50);
+    let end = Point::new(53, 50);
+    let result = plot_line(start, end);
+    let expected_result = vec![
+      Point::new(50, 50),
+      Point::new(51, 50),
+      Point::new(52, 50),
+      Point::new(53, 50),
+    ];
+
+    assert_eq!(result, expected_result, "Should have been equal");
+  }
+
+  #[test]
+  pub fn should_draw_line_inbetween_points_4() {
+    let start = Point::new(50, 50);
+    let end = Point::new(50, 50);
+    let result = plot_line(start, end);
+    let expected_result = vec![Point::new(50, 50)];
+
+    assert_eq!(result, expected_result, "Should have been equal");
+  }
+
+  #[test]
+  pub fn should_draw_line_inbetween_points_5() {
+    let start = Point::new(50, 50);
+    let end = Point::new(55, 51);
+    let result = plot_line(start, end);
+    let expected_result = vec![
+      Point::new(50, 50),
+      Point::new(51, 50),
+      Point::new(52, 50),
+      Point::new(53, 51),
+      Point::new(54, 51),
+      Point::new(55, 51),
+    ];
+
+    assert_eq!(result, expected_result, "Should have been equal");
   }
 }
